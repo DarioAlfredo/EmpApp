6b55569bfb1be72cafe76d74d8c6d4b059ac0955
dario <dariohoa@gmail.com>
2025-06-20 20:03:23 +0200

    Validate data

--- EmpApp/Components/Models/EmployeeDto.cs
+++ EmpApp/Components/Models/EmployeeDto.cs
@@ -1,12 +1,26 @@
-﻿namespace EmpApp.Components.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmpApp.Components.Models
 {
     public class EmployeeDto
     {
         public int EmployeeID { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
         public int? EmployeeTitleID { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(100, ErrorMessage = "First name cannot exceed 100 characters.")]
         public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(100, ErrorMessage = "Last name cannot exceed 100 characters.")]
         public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Date of birth is required.")]
+        [DataType(DataType.Date)]
         public DateTime DOB { get; set; }
+
         public bool IsActive { get; set; }
         public string? EmployeeTitle { get; set; } // For joined title
     }
